922c8f8df9758af9165ffa2bf07ba7ab09a54c15
preyas <preyas.rana@zymr.com>
2022-05-26 17:11:01 +0530

    msg

--- src/test/java/API_Tests/GetandPost_Examples.java
+++ src/test/java/API_Tests/GetandPost_Examples.java
@@ -6,56 +6,59 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
 import java.util.Map;
 import org.apache.commons.collections4.map.HashedMap;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 public class GetandPost_Examples {
-	
-	//@Test
+
+	// @Test
 	public void getname() {
-		
+
 		baseURI = "https://reqres.in/api";
-		
-		given()
-		   .get("/users?page=2")
-		.then()
-		    .statusCode(200)
-		    .body("data[0].first_name", equalTo("Michael"))
-		    .body("data.first_name", hasItems("Michael,Lindsay,Tobias,Byron,George"));
-		
+
+		given().get("/users?page=2").then().statusCode(200).body("data[0].first_name", equalTo("Michael"))
+				.body("data.first_name", hasItems("Michael,Lindsay,Tobias,Byron,George"));
+
 	}
-	
-	@Test
+
+	// @Test
 	public void testpost() {
-		
-		Map<String, Object> map = new HashedMap<String,Object>();
-		//map.put("name", "preyas");
-		//map.put("job", "test");
-		
-		//System.out.println(map);
-		
-		JSONObject  request = new JSONObject(map);
+
+		Map<String, Object> map = new HashedMap<String, Object>();
+		// map.put("name", "preyas");
+		// map.put("job", "test");
+
+		// System.out.println(map);
+
+		JSONObject request = new JSONObject(map);
 		request.put("name", "preyas");
 		request.put("job", "test");
-			
+
 		System.out.println(request.toJSONString());
-		
+
 		baseURI = "https://reqres.in/api";
-		
-		given()
-		.header("Content-Type","application/json")
-		.contentType(ContentType.JSON)
-		   .body(request.toJSONString())
-		   .when()
-		   .post("/users")
-		   .then().statusCode(201)
-		   
-		   .log().all();
-		
+
+		given().header("Content-Type", "application/json").contentType(ContentType.JSON).body(request.toJSONString())
+				.when().post("/users").then().statusCode(201)
+
+				.log().all();
+
 	}
-	
-	
+
+	@Test
+	public void get() {
+
+		given()
+		   .when()
+		   .get("https://flask-rest-api-demo.herokuapp.com/product/motorbike")
+		.then().statusCode(200)
+				.statusLine("HTTP/1.1 200 OK")
+		.header("Content-Type", "application/json")
+		.log().all();
+
+	}
 
 }
--- src/test/java/API_Tests/Restassured.java
+++ src/test/java/API_Tests/Restassured.java
@@ -1,9 +1,14 @@
 package API_Tests;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.*;
+
 import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.http.Method;
@@ -11,6 +16,17 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+
+/*
+  given()
+      set cookies , add auth , param, header etc
+  when()
+     get,put,post,delete
+  then()
+    validate status code, exract code , cookies etc          
+ */
 
 public class Restassured {
 
@@ -26,7 +42,7 @@ public class Restassured {
 
 	}
 
-	//@Test
+	// @Test
 	public void name() {
 
 		RestAssured.baseURI = "https://reqres.in/api";
@@ -49,43 +65,66 @@ public class Restassured {
 			System.out.println("Key ::" + header.getName() + "   ,Value ::" + header.getValue());
 
 		}
-		
-		String contenttype  = res.header("Content-Type");
-		System.out.println("Content type is ::"+contenttype);
-		
+
+		String contenttype = res.header("Content-Type");
+		System.out.println("Content type is ::" + contenttype);
+
 		String server = res.header("Server");
-		System.out.println("Server value is ::"+server);
-		
+		System.out.println("Server value is ::" + server);
+
 		ResponseBody body = res.getBody();
-		System.out.println("body response is"+body.asString());
-		
+		System.out.println("body response is" + body.asString());
+
 		JsonPath jsonpath = res.jsonPath();
-		
+
 		int total = jsonpath.get("total");
-		System.out.println("Total value is ::"+total);
-		System.out.println("Total value is ::"+String.valueOf(total));
+		System.out.println("Total value is ::" + total);
+		System.out.println("Total value is ::" + String.valueOf(total));
 
 	}
-	
-	
-	@Test
+
+	// @Test
 	public void queryparam() {
-		
+
 		RestAssured.baseURI = "https://demoqa.com/BookStore/v1";
-		
+
 		RequestSpecification httpsreq = RestAssured.given();
-		Response res = httpsreq.queryParam("ISBN","9781449325862").get("/Book");
-		ResponseBody body = res.getBody();	
-		System.out.println("body response is"+body.asString());
-		
+		Response res = httpsreq.queryParam("ISBN", "9781449325862").get("/Book");
+		ResponseBody body = res.getBody();
+		System.out.println("body response is" + body.asString());
+
 		String bodyres = body.asString();
-		
+
 		JsonPath jsonpath = new JsonPath(bodyres);
 		System.out.println(jsonpath);
-		
+
 		String title = jsonpath.getString("title");
 		System.out.println(title);
-		
+
 	}
 
+	//@Test
+	public void basicauth() 
+	   {
+
+		String Username = "Testing_new_user23";
+		String Password ="Test10@Preyas";
+		String isbn = "9781593277574";
+		String baseUrl = "https://bookstore.demoqa.com";
+		String endpoint = "/Account/v1/User";
+		
+		
+		RestAssured.baseURI = baseUrl;
+		RequestSpecification httpreq = given().auth().basic(Username,Password);
+		Response res = httpreq.get(endpoint);
+
+		ResponseBody body = res.body();
+		String resnew = body.asString();
+
+		System.out.println("Response ::" + body.asPrettyString());
+		System.out.println("resnew  ::" + resnew);
+
+	}
+
+	
 }
